aa0dc5da5c4e596673cea2e469dd5ffa74b9bc48
Óscar Cabezas <turboveja@gmail.com>
2021-04-22 12:53:26 +0200

    Commit inicial

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+/**
+ * Class AuthServiceProvider
+ * @package App\Providers
+ */
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //Rutas Passport
+        Passport::routes();
+
+        //Configuración tokens Passport
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+    }
+}
